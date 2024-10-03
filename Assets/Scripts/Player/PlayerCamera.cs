49a0ca63ff9e207e9ff043f8f918b227d2edec43
watsson <ringganovalll@gmail.com>
2024-10-03 18:54:36 +0700

    feat: add camera follow the player

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player; // referensi ke pemain
-    public Vector3 offset;   // jarak antara kamera dan pemain
+    public Transform player; 
+    public Vector3 offset;   
 
     void Update()
     {
         if (player != null)
         {
-            // Update posisi kamera sesuai dengan posisi pemain ditambah offset
             transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
         }
     }
